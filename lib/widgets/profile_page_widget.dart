5409de466a1e73d6e335e8d9d8d2e720c6452c50
Yugesh Poudel <yugman45d@gmail.com>
2023-07-01 13:38:15 +0545

    Made the Profile Page Dynamic

---
+++ lib/widgets/profile_page_widget.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:instagram/datas/profile_page_data.dart';
+import 'package:instagram/models/profile_page_model.dart';
+
+class Profile_Page_Widget extends StatelessWidget {
+  const Profile_Page_Widget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ...profilePageData.map(
+          (e) => Profile_Page_Model(
+            profile_pic: e['profile_pic'],
+            username: e['username'],
+            name: e['name'],
+            posts: e['posts'],
+            followers: e['followers'],
+            following: e['following'],
+            bio: e['bio'],
+          ),
+        ),
+      ],
+    );
+  }
+}
