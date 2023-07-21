717d2b180515d1319d9a542b9f6097bd99d0ec67
Yugesh Poudel <yugman45d@gmail.com>
2023-07-21 17:52:45 +0545

    Nav bar UI updated

--- lib/screens/mainpages/main_page.dart
+++ lib/screens/mainpages/main_page.dart
@@ -14,6 +14,11 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  bool home = true;
+  bool search = false;
+  bool add = false;
+  bool reels = false;
+  bool avatar = false;
   int selected_index = 0;
   List<Widget> pages = [
     Insta_HomePage(),
@@ -30,66 +35,98 @@ class _MainPageState extends State<MainPage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        
         body: getBody(),
         bottomNavigationBar: BottomNavigationBar(
-          selectedItemColor: Colors.black,
-          selectedIconTheme: IconThemeData(),
           currentIndex: selected_index,
           onTap: (newIndex) {
             setState(() {
               selected_index = newIndex;
+              switch (selected_index) {
+                case 0:
+                  {
+                    home = true;
+                    search = false;
+                    add = false;
+                    reels = false;
+                    avatar = false;
+                  }
+                case 1:
+                  {
+                    home = false;
+                    search = true;
+                    add = false;
+                    reels = false;
+                    avatar = false;
+                  }
+                case 2:
+                  {
+                    home = false;
+                    search = false;
+                    add = true;
+                    reels = false;
+                    avatar = false;
+                  }
+                case 3:
+                  {
+                    home = false;
+                    search = false;
+                    add = false;
+                    reels = true;
+                    avatar = false;
+                  }
+                case 4:
+                  {
+                    home = false;
+                    search = false;
+                    add = false;
+                    reels = false;
+                    avatar = true;
+                  }
+              }
             });
           },
           type: BottomNavigationBarType.fixed,
           items: <BottomNavigationBarItem>[
             BottomNavigationBarItem(
-              // icon: Image.asset(
-              //   "assets/home_filled.png",
-              // height: 28,
-              // ),
-              icon: Icon(
-                Icons.home_filled,
-                size: 28,
-              ),
+              icon: home
+                  ? Image.asset("assets/home_filled.png", height: 28)
+                  : Image.asset("assets/home.png", height: 28),
               label: "",
             ),
             BottomNavigationBarItem(
-              icon: Icon(
-                Icons.search,
-                size: 28,
-              ),
+              icon: search
+                  ? Icon(Icons.search, size: 32, color: Colors.black)
+                  : Image.asset("assets/search.png", height: 28),
               label: "",
             ),
             BottomNavigationBarItem(
-              // icon: Image.asset(
-              //   "assets/add.png",
-              //   height: 28,
-              // ),
-              icon: Icon(
-                Icons.add_box,
-                size: 28,
-              ),
+              icon: add
+                  ? Image.asset("assets/add_filled.png", height: 28)
+                  : Image.asset("assets/add.png", height: 28),
               label: "",
             ),
             BottomNavigationBarItem(
-              // icon: Image.asset(
-              //   "assets/reels.png",
-              //   height: 32,
-              // ),
-              icon: Icon(
-                Icons.video_collection_rounded,
-                size: 28,
-              ),
+              icon: reels
+                  ? Image.asset("assets/reels_filled.png", height: 28)
+                  : Image.asset("assets/reels.png", height: 28),
               label: "",
             ),
             BottomNavigationBarItem(
-              icon: CircleAvatar(
-                radius: 16,
-                backgroundImage: AssetImage(
-                  "assets/1.jpg",
-                ),
-              ),
+              icon: avatar
+                  ? Stack(alignment: Alignment.center, children: [
+                      CircleAvatar(
+                        radius: 16,
+                        backgroundColor: Colors.black,
+                      ),
+                      CircleAvatar(
+                        radius: 14,
+                        backgroundImage: AssetImage("assets/1.jpg"),
+                      ),
+                    ])
+                  : CircleAvatar(
+                      radius: 16,
+                      backgroundImage: AssetImage("assets/1.jpg"),
+                    ),
               label: "",
             ),
           ],
